7af7c2d86612936eb393d7fa9fb9682492389414
Kevin D. Rollins <weblog@kevinrollins.com>
2021-11-04 15:47:26 -0400

    add link to Anik's blog on ImperativeHandle

--- src/ImpHandle.js
+++ src/ImpHandle.js
@@ -1,1 +1,7 @@
-import React from 'react'; 
+import React from 'react';
+
+export default function ImpHandle() {
+  return <>
+          <p>First we need to read this <a href="https://dev.to/anikcreative/react-hooks-explained-useimperativehandle-5g44">article by Anik</a></p>
+         </>;
+}
